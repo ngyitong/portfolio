78774d1928146a0c9e44f4853f02ae15fb2f9b1b
PlacidPlaty <yitongng4.0@gmail.com>
2024-12-15 23:22:39 +0800

    to add profile pic and intro

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import './global.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      
+      <header className="App-header">
+        <h1>Hello world!</h1>
+      </header>
+
+      <div className="background">
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
+
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload?
+//     </p>
+
+//     {/* create a hyperlink */}
+//     <a
+//       className="App-link"
+//       href="https://reactjs.org"
+//       target="_blank"
+//       rel="noopener noreferrer"
+//     >
+//       Learn React
+//     </a>
+//   </header>
+// </div>
