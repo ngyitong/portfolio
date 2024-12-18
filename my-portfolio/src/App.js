76d078ea9766b582c35d78205b744e5321be4ff0
PlacidPlaty <yitongng4.0@gmail.com>
2024-12-19 00:15:27 +0800

    changed footer a bit

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -7,18 +7,66 @@ function App() {
     <div className="app">
     
       <div className="background">
-
         <header className="header">
           <div className="flex-container-row">
-            <h3>Hello world!</h3> 
-            <h3>Bye worlds!</h3>
+            <h3 className={ "heading-font align-left" }>Ng Yi Tong</h3> 
+            <h3 className={ "heading-font align-right" }>Projects</h3>
+            <h3 className={ "heading-font" }>Projects2</h3>
+            <h3 className={ "heading-font" }>Projects3 </h3>
           </div>
         </header>
 
-        <footer className="footer">
-          <h3>wordss</h3>
-        </footer>
       </div>
+        <div className="footer">
+          <div className="flex-container-row"> 
+            <div className="heading-font">My links</div>
+            <div className="flex-break"></div>
+            <a 
+              className="links-font"
+              href="https://github.com/PlacidPlaty"
+              // prevent new page from using JS to modify or redirect the original page
+              rel="noopener noreferrer"
+              // opens link in a new tab or window
+              target="_blank"
+            >
+              GitHub
+            </a>
+
+            <a 
+              className="links-font"
+              href="https://www.linkedin.com/in/ng-yi-tong/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
+
+            <div className="flex-break"></div>
+
+{/* Credits for background */}
+            <div className="small-font">
+              Background photo by
+                <a className="app-link small-font"
+                  href="https://unsplash.com/@sametomorrow?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Adam Chang
+                </a>
+
+              on
+              <a className="app-link small-font"
+                  href="https://unsplash.com/photos/snow-field-and-green-pine-trees-during-daytime-IWenq-4JHqo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  Unsplash
+                </a>
+              
+            </div>
+          </div>
+
+        </div>
 
     </div>
   );
--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/Open_Sans/static/OpenSans-Regular.ttf'
+import './fonts/Open_Sans/static/OpenSans_Condensed-Italic.ttf'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
