7d4c54a84c70fb2b0fab6e0474ef371a5a8ee620
ngyitong <yitongng4.0@gmail.com>
2025-01-12 19:00:41 +0800

    updated my project and experiences

--- my-portfolio/src/components/displayData.js
+++ my-portfolio/src/components/displayData.js
@@ -5,6 +5,9 @@ Data will be in a list.
 
 import googleCert from '../pictures/Coursera-Google-cybersec-cert.png';
 import brainhackCert from '../pictures/brainhack-2024.png';
+// import cs440ProjectPic1 from "../pictures/cs440-project-pic1.jpeg";
+import cs440ProjectPic2 from "../pictures/cs440-project-pic2.png";
+
 
 
 /**
@@ -60,14 +63,16 @@ export function getExperiences() {
             body: "This certificate introduced me to the fundamentals of cybersecurity. " + 
                     "I learnt about the different tools used by cybersecurity professionals to prevent, detect and respond to cyber attacks. " +
                     "This course also taught me how a company can identify, manage and respond to cyberseucity risks. " + 
-                    "Additionally, I was given hands on experience with tools such as Wireshark, tcpdump and VirusTotal." + 
-                    "My completion of this course has provided me with a better appreciation of the importance of cybersecurity and the motivation to learn more about the different fields in cybersecurity.",
+                    "Additionally, I was given hands on experience with tools such as Wireshark, tcpdump and VirusTotal. " + 
+                    "My completion of this course has provided me with a better appreciation of the importance of cybersecurity and the motivation to learn more about the different fields in cybersecurity. ",
             imagePath: googleCert
         },
         {
             id: '1',
             title: "DSTA Brainhack 2024",
-            body: "I participated in the Brainhack cybersecurity hackathon, with a team of 4. ",
+            body: "I participated in the Brainhack cybersecurity hackathon with a team of 4. " +
+                  "This hackathon presented numerous challenges which helped me to develop my techinical skills, " + 
+                  "espcially in network security and linux commands. ",
             imagePath: brainhackCert
         }
     ]
@@ -82,14 +87,22 @@ export function getProjects() {
     // id is added so that html does not complain when displaying the list
         {
             id: '0',
-            title: "This porfolio page!",
+            title: "This portfolio page!",
             body: "I created this webpage with the goal of learning more about frontend and ReactJS. " +
-                  "I employed external CSS to style each components and for better code reusability. "
+                  "I employed external CSS to style each components for better code reusability. "
         },
         {
             id: '1',
-            title: "My second project",
-            body: "My explanation goes here 2"
+            title: "Implementation of Ransomware (University Project) ",
+            body: "This project was part of my university course work. Collaborating with a team of 4, we created a ransomware that is deployed via Raspberry Pi Pico. " + 
+                  "Raspberry Pi Pico acted as an external keyboard, when inserted into a Windows device, " + 
+                  "would automatically download our ransomware code and execute it. " +
+                  "\n\nA hardware-based attack was choosen as allows us to bypass the security measures that are in place in browsers. " +
+                  "Our ransomware code was written in Python, which is able to look for specific file types and encrypt them. " +
+                  "After encrypting the files, a popup would appear on the screen, demanding a ransom from the user. " + 
+                  "Using webhook, it would send the decryption key to our server via hybrid encryption. " + 
+                  "Through this project, I was able to gain a better understanding of how ransomware works through the perspective of an attacker. ",
+            imagePath: cs440ProjectPic2
         }
     ]
 
